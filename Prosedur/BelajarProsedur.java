e4cfe4f9d5b532fb7708db3125be8dcbfcda843e
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2023-12-23 12:20:37 +0700

    update commit

---
+++ Prosedur/BelajarProsedur.java
@@ -0,0 +1,13 @@
+class BelajarFungsi {
+    
+    // membuat fungsi ucapSalam()
+    static void ucapSalam(){
+        System.out.println("Selamat Pagi");
+    }
+
+    // membuat fungsi main()
+    public static void main(String[] args){
+        // memanggil/eksekusi fungsi ucapSalam()
+        ucapSalam();
+    }
+}
